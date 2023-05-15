3f1f524283659937797aaea8883a5baaab5cab36
EDU4RDO-DEV <joseguzman20162020@gmail.com>
2023-05-15 17:11:03 -0600

    Se inicio la API de Sistema de Ventas

---
+++ spring-boot-backend-apirest-sales-system/src/main/java/com/umg/springboot/backend/apirest/SpringBootBackendApirestSalesSystemApplication.java
@@ -0,0 +1,13 @@
+package com.umg.springboot.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendApirestSalesSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendApirestSalesSystemApplication.class, args);
+	}
+
+}
---
+++ spring-boot-backend-apirest-sales-system/src/test/java/com/umg/springboot/backend/apirest/SpringBootBackendApirestSalesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umg.springboot.backend.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendApirestSalesSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
