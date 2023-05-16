ba2f86c3e6d783bb72afa3434da021eb24303c7c
EDU4RDO-DEV <joseguzman20162020@gmail.com>
2023-05-15 19:03:51 -0600

    Clase Empleado creada.

---
+++ spring-boot-backend-apirest-sales-system/src/main/java/com/umg/springboot/backend/apirest/models/entity/Empleado.java
@@ -0,0 +1,78 @@
+package com.umg.springboot.backend.apirest.models.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="empleados")
+public class Empleado implements Serializable{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int codigo_Empleado;
+	
+	private int tipoEmpleado_Id_TipoEmpleado;
+	private int almacen_Id_Almacen;
+	private String usuario;
+	private String clave;
+	private String estado;
+	
+	public int getCodigo_Empleado() {
+		return codigo_Empleado;
+	}
+
+	public void setCodigo_Empleado(int codigo_Empleado) {
+		this.codigo_Empleado = codigo_Empleado;
+	}
+
+	public int getTipoEmpleado_Id_TipoEmpleado() {
+		return tipoEmpleado_Id_TipoEmpleado;
+	}
+
+	public void setTipoEmpleado_Id_TipoEmpleado(int tipoEmpleado_Id_TipoEmpleado) {
+		this.tipoEmpleado_Id_TipoEmpleado = tipoEmpleado_Id_TipoEmpleado;
+	}
+
+	public int getAlmacen_Id_Almacen() {
+		return almacen_Id_Almacen;
+	}
+
+	public void setAlmacen_Id_Almacen(int almacen_Id_Almacen) {
+		this.almacen_Id_Almacen = almacen_Id_Almacen;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getClave() {
+		return clave;
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
