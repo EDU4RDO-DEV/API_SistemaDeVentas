4e65538ad6a1a4fe0bb2876e260df5177d9228b1
allan <allan25chub@gmail.com>
2023-05-23 00:09:15 -0600

    Cambios, se le quitó la tilde a la palabra DirecciónAlmacen

--- spring-boot-backend-apirest-sales-system/src/main/java/com/umg/springboot/backend/apirest/models/entity/Almacen.java
+++ spring-boot-backend-apirest-sales-system/src/main/java/com/umg/springboot/backend/apirest/models/entity/Almacen.java
@@ -18,7 +18,7 @@ public class Almacen implements Serializable{
 	private int Id_Almacen;
 	
 	private String Nombre_almacen;
-	private String Dirección_almacen;
+	private String Direccion_almacen;
 	private String Telefono_almacen;
 	private char Estado;
 	
@@ -38,13 +38,6 @@ public class Almacen implements Serializable{
 		Nombre_almacen = nombre_almacen;
 	}
 
-	public String getDirección_almacen() {
-		return Dirección_almacen;
-	}
-
-	public void setDirección_almacen(String dirección_almacen) {
-		Dirección_almacen = dirección_almacen;
-	}
 
 	public String getTelefono_almacen() {
 		return Telefono_almacen;
@@ -60,6 +53,14 @@ public class Almacen implements Serializable{
 
 	public void setEstado(char estado) {
 		Estado = estado;
+	}
+
+	public String getDireccion_almacen() {
+		return Direccion_almacen;
+	}
+
+	public void setDireccion_almacen(String direccion_almacen) {
+		Direccion_almacen = direccion_almacen;
 	}
 
 	/**
